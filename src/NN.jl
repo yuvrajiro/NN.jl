5e4ba30feb4a86f51009889c86a8bf89f1f81dd1
Rahul Goswami <yuvrajiro@gmail.com>
2021-01-17 02:34:46 +0530

    mm

--- src/NN.jl
+++ src/NN.jl
@@ -6,11 +6,22 @@ include("activation.jl")
 
 using LinearAlgebra
 
-struct NN
-    layer::Array{Int64,1}
+struct perceptron
+  activation::Function
 end
 
-function compute(N::NN,W::Array{Array{Float64,2},1},Input::Array{Float64,1})
+struct layer
+  layer::Array{perceptron,1}
+end
 
+struct NN
+  NN::Array{layer,1}
+end
+
+
+perceptron_compute(x::perceptron,W::Array{Float64,1},I::Array{Float64,2}) = x.activation.(I * W)
+layer_compute(l::layer,W::Array{Float64,2},I::Array{Float64,2}) = [pcom(i,W[:,j],I) for (i,j) in zip(l.layer,1:size(W,2)]
+
+NeuralNetwork(x::Array{Tuple{Function,Int64},1}) = NN([layer([perceptron(x[i][1]) for k in 1:(x[i][2]+1)]) for i in 1:length(x)])
 
 end
